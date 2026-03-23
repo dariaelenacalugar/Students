92275c4ee96edc1c067c372b69be0e810c755c95
Daria Calugar <dariaelena.calugar@ulbsibiu.ro>
2026-03-23 14:43:56 +0200

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import ro.ulbs.proiectaresoftware.students.Student;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+        List<Student> listaStudenti=new ArrayList<Student>();
+        listaStudenti.add(new Student(112, "Ioan", "Popa", "TI21/1"));
+        listaStudenti.add(new Student(112, "Maria", "Oprea", "TI21/1"));
+        listaStudenti.add(new Student(120,"Alis","Popa","TI21/2"));
+        listaStudenti.add(new Student(122, "Mihai", "Vercedea", "TI22/1"));
+        listaStudenti.add(new Student(122, "Eugen", "Uritescu", "TI22/2"));
+
+        System.out.println("Lista studenti: ");
+        for(Student s:listaStudenti){
+            System.out.println(s);
         }
+        Student s1=new Student(120,"Alis","Popa","TI21/2");
+        boolean containsStudent=verificaStudent(listaStudenti,s1);
+        System.out.println(containsStudent);
+
+
+    }
+    public static boolean verificaStudent(List<Student>listaStudenti,Student s1)
+    {
+        for(Student s:listaStudenti){
+            if(s.getNume().equals(s1.getNume())&&s.getPrenume().equals(s1.getPrenume())&&s.getFormatieDeStudiu().equals(s1.getFormatieDeStudiu())){
+                return true;
+            }
+        }
+        return false;
     }
 }
